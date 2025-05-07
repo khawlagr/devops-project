7357354d6eca06be2f4d62a1c07626acad1fa817
unknown <khawlagrissa@gmail.com>
2025-05-07 15:29:46 +0200

    Commit Terraform

---
+++ terraform/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+
+
+resource "docker_image" "myapp" {
+  name = "myapp"
+  build {
+    context = "${path.module}/.."
+  }
+}
+
+resource "docker_container" "myapp" {
+  name  = "myapp"
+  image = docker_image.myapp.latest
+
+  ports {
+    internal = 3000
+    external = 3000
+  }
+}
